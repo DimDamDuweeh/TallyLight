6f0d4fe03e097a1b0477bed719fd220ea56dada9
DimDamDuweeh <48521467+DimDamDuweeh@users.noreply.github.com>
2019-05-16 13:23:07 +0200

    Werkende versie met begin aan if statements

--- TheThreadTallyLight.py
+++ TheThreadTallyLight.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-import time
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 8099
@@ -11,43 +10,45 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((TCP_IP, TCP_PORT))
 
 
-def sendMsg(socket):
-    try:
-        while True:
-            socket.send(bytes("TALLY\r\n", 'utf-8'))
-            time.sleep(2)
+def get_data(socket):
+    while True:
+        try:
+            data = socket.recv(MSG_SIZE)
+            data = str(data, 'utf-8')
+            antwoord = data.split(' ')
+            print(antwoord[2])
+            tallyIndex(antwoord[2])
 
-    except Exception as e:
-        print(e)
-    finally:
-        socket.close()
+            index = tallyIndex(antwoord[2])
+            tallySwitcher(index)
+
+            if not data:
+                break
+        except socket.error as e:
+            print(e)
+            break
+
 
 # Maakt thread aan voor socket connectie
-iThread = threading.Thread(target=sendMsg, args=(socket,))
+iThread = threading.Thread(target=get_data, args=(socket,))
 iThread.start()
 print("Connected...")
 
 
-
-def get_data():
-    while True:
-        try:
-            data = socket.recv(MSG_SIZE)
-            data = data[9:]
-            print(str(data, 'utf-8'))
-            if not data:
-                break
-
-        except socket.error:
-            print("error occured.")
-            break
-
-    while True:
-        get_data()
-        break
-
-def tallyswitcher():
-    for x in range(len(get_data().data)):
-        print("hallo")
+def tallyIndex(response):
+    try:
+        # print uit op welke index de camera die live is staat
+        print(response.index("1"))
+    except Exception as e:
+        print(e)
+    finally:
+        print(response)
+        return response
 
 
+def tallySwitcher(switch):
+    if switch == "1":
+        print("kappa")
+
+
+socket.send(bytes("SUBSCRIBE TALLY\r\n", 'utf-8'))

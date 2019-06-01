272867bb7b5226c8ef682cfdb7c9e216d61dc4e3
DimDamDuweeh <48521467+DimDamDuweeh@users.noreply.github.com>
2019-06-01 10:46:57 +0200

    TallyAan functie update

--- app.py
+++ app.py
@@ -1,52 +1,35 @@
-from flask import Flask, render_template, request
+# @Author Danja Verburg
+# flask app met app.routes die worden verwerkt in GUI
+
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/ ', methods=['POST'])
-def hello_world(value):
-    btn1 = False
-
-    if request.method == 'POST':
-        if value == "0":
-            btn1 = True
-
-
-    if request.method == 'POST':
-        if request.form['lamp'] == 'on':
-            btn1 = True
-        elif request.form['value'] == 'off':
-            btn1 = False
-    return render_template("UserInterface.html")
+@app.route('/')
+def hello_world():
+    return render_template("test.html")
 
 
 @app.route('/tallyAAN=<tallyAAN>')
 def tallyAAN(tallyAAN):
-    lengte = len(tallyAAN)
-    lengte = lengte - 2
-    print(lengte)
-    return str(lengte)
+    lengte = tallyAAN.split("\\")[0]
+    print(len(lengte))
+    return lengte
 
 
 @app.route('/tally=<tally>')
 def tally(tally):
-    tally1 = False
-    tally2 = False
-    tally3 = False
-
+    print(tally)
     if tally == "0":
-        tally1 = True
         print("taart")
     elif tally == "1":
-        tally2 = True
         print("taart is")
     elif tally == "2":
-        tally3 = True
         print("taart is lekker")
 
-        return render_template("index.html", tally1=tally1, tally2=tally2, tally3=tally3)
-
+    return render_template("test.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
